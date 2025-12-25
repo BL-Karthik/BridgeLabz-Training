052e2e46bf85ec91acecbc7dcc73be6f7bb0c1f4
KarthikNani18 <nanikarthik7059@gmail.com>
2025-12-26 00:14:59 +0530

    [karthik[] :a edd

---
+++ oops-java/oopsconceptsproject/src/com/bridgelabz/encapsulation/Student.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.encapsulation;
+
+public class Student {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+          System.out.println("hi Shilpa");
+	}
+}
