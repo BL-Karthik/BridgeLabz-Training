629c2049c3705f13cee543ab820111d8db18d825
KarthikNani18 <nanikarthik7059@gmail.com>
2025-12-26 11:04:03 +0530

    [Karthik] : added initial setup

--- oops-java/oopsconceptsproject/src/com/bridgelabz/encapsulation/Student.java
+++
@@ -1,9 +0,0 @@
-package com.bridgelabz.encapsulation;
-
-public class Student {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-          System.out.println("hi Shilpa");
-	}
-}
